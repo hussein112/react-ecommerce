0a380b19e0da528099fd388beec25c88b3353e67
hussein khalil <husseinkhalil420@gmail.com>
2024-02-21 03:08:16 +0200

    created Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
